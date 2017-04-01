adbd7421a6d38db339654b3c2b49340bbce7c474
Rico11112016 <ph3@wp.pl>
2017-04-01 17:26:58 +0200

    $routeProvider added to app.module

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 var app = angular.module('CalendarApp', ['ngRoute']);
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            controller: "DaysController",
+            templateUrl: "views/days.html"
+        })
+        .otherwise({
+            redirecTo: "/"
+        });
+});
